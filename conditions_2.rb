c2b3ac68a41699ff9963b3c19638ffdbb100c05d
Zacbouu <zacharia_ha@yahoo.fr>
2023-08-11 18:45:06 +0200

    add version of condition with elsif

---
+++ conditions_2.rb
@@ -0,0 +1,12 @@
+age = 36
+if age > 17 && age == 36
+  puts "cette personne est majeur !"
+  puts "tu as mon age"
+elsif age > 17 && age == 21
+    puts "cette personne est majeur !"
+    puts "cette personne à 21 ans"  
+elsif age > 17
+  puts "cette personne est majeur !"
+else  
+  puts "cette personne est mineur"
+end
