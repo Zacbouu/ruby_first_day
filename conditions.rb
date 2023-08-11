aee2f89cf0f30ddeb474315fece6a85d7317fda0
Zacbouu <zacharia_ha@yahoo.fr>
2023-08-11 18:00:21 +0200

    add ruby conditions course from thp

---
+++ conditions.rb
@@ -0,0 +1,13 @@
+age = 36
+if age > 17
+  puts "cette personne est majeur !"
+end
+if age == 21
+  puts "cette personne à 21 ans"
+end
+if age < 17
+  puts "cette personne est mineur"
+end
+if age == 36
+  puts "tu as mon age"
+end
