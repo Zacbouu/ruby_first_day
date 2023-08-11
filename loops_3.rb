97ad7b33528a196b6734f4031b0bc8df328b0dd4
Zacbouu <zacharia_ha@yahoo.fr>
2023-08-11 18:46:44 +0200

    add ruby loops course from thp

---
+++ loops_3.rb
@@ -0,0 +1,6 @@
+5.times do |i|
+  puts "=== Boucle n°#{i + 1} ==="
+  5.times do |j|
+    puts j + 1
+  end
+end
---
+++ loops_4.rb
@@ -0,0 +1,6 @@
+5.times do |i|
+  puts "=== Boucle n°#{i + 1} ==="
+  i.times do |j|
+    puts j + 1
+  end
+end
